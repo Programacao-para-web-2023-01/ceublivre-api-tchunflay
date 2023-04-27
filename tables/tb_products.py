494ef4b1e9885b999b744c15ac05279e16a114e3
Lucca Paiva <paiva.rc.lucca@gmail.com>
2023-04-27 10:02:33 -0300

    Update tb_products.py

--- tables/tb_products.py
+++ tables/tb_products.py
@@ -18,7 +18,7 @@ engine = create_engine("sqlite:///database.db")
 def create_database():
     SQLModel.metadata.create_all(engine)
 
-# insert into database with session
+# insert data with session
 def insert_data():
     product1 = Products(category = "Videogame", brand = "Sony", name = "PS5", description = "Console mídia física + DUALSHOCK", rate = 4.7, price = 4599.99, image = "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg")
     product2 = Products(category = "Videogame", brand = "Sony", name = "PS5", description = "Console mídia física + DUALSHOCK", rate = 4.3, price = 4999.99, image = "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg")
