bb19590d23f1ca0f9ac344b9f487d80ba262f714
Lucca Paiva <paiva.rc.lucca@gmail.com>
2023-06-29 00:25:16 -0300

    Update tb_products.py
    updated database

--- tables/tb_products.py
+++ tables/tb_products.py
@@ -17,6 +17,10 @@ class Products(SQLModel, table=True):
     price: float
     image: str
 
+class Category(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+
 #create database based on above sqlmodel
 engine = create_engine(connection_string, echo=True)
 
@@ -25,13 +29,34 @@ def create_database():
 
 # insert data with session
 def insert_data():
-    product1 = Products(category = "Videogame", brand = "Sony", name = "PS5", description = "Console mídia física + DUALSHOCK", rate = 4.7, price = 4599.99, image = "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg")
-    product2 = Products(category = "Videogame", brand = "Sony", name = "PS5", description = "Console mídia física + DUALSHOCK", rate = 4.3, price = 4999.99, image = "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg")
+    categories = ["Gaming", "Livros", "Papelaria", "Acessórios", "Roupa Masculina", "Roupa Feminina", "PetShop", "Eletrônicos", "Esportes"]
+    
+    with Session(engine) as session:
+        for category in categories:
+            session.add(Category(name=category))
+            session.commit()
+
+
+    products = [("Gaming", "Sony", "PS5", "Console mídia física + DualSense", 4.7, 4599.99, "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg"),
+    ("Gaming", "Sony", "PS5", "Console mídia física + DualSense", 4.3, 4999.99, "C:\program_web\database\5IjvCH5cYUSF8ePcjJm1KQ==.jpg"),
+    ("Gaming", "Microsoft", "Xbox Series S", "Console Xbox Series S 512GB", 4.5, 3699.99, "C:\program_web\database\xbox.jpg"),
+    ("Livros", "Rocco", "Jogos Vorazes", "Livro Juvenil Fantasia", 4.4, 42.99, "C:\program_web\database\jogos-vorazes.jpg"),
+    ("Livros", "Intrinseca", "Percy Jackson e os Olimpianos BUNDLE", "Série de Livros Percy Jackson e os Olimpianos, Juvenil Mitologia", 4.8, 412.63, "C:\program_web\database\percyjackson.jpg"),
+    ("Papelaria", "Faber Castell", "Caixa de Lápis de Cor Pequena", "Lápis de Cor Faber Castell 12 Cores + 6 Cores Neon", 4.2, 23.54, "C:\program_web\database\lapis.jpg"),
+    ("Papelaria", "Tilibra", "Caderno Tilibra Universe", "Caderno Tilibra 96 páginas, folha branca", 3.8, 17.90, "C:\program_web\database\caderno.jpg"),
+    ("Acessórios", "Doxa", "Relógio Doxa Aquamarine", "Relógio Doxa Aquamarine Diver Masculino, tamanho ajustável prata", 3.6, 245.50, "C:\program_web\database\doxa.jpg"),
+    ("Acessórios", "Bontrager", "Luvas Bontrager Velocis", "Luvas Pretas Ciclismo Masculina Dedo Longo", 4.0, 290.00, "C:\program_web\database\luvas.jpg"),
+    ("Acessórios","-", "Anel de Cruz","Anel Prata de Cruz Retrô Tamanho Ajustável" ,2.3 ,173.70 ,"C:\program_web\database\anel.jpg"),
+    ("Roupa Masculina","Levis","Bermuda Jeans Levis","Bermuda Masculina Jeans Levis Tamanho M" ,3.4 ,179.90 ,"C:\program_web\database\berm.jpg"),
+    ("Roupa Feminina","Western","Bota Feminina Western Preto","Bota Feminina Cano Alto Preta Western 37" ,3.7 ,329.90 ,"C:\program_web\database\botaf.jpg"),
+    ("Roupa Feminina","Carlos Miele","Vestido Longo Goiaba","Vestido Curto Carlos Miele Goiaba M" ,3.9 ,420.99 ,"C:\program_web\database\vest.jpg")
+    ]
 
     with Session(engine) as session:
-        session.add(product1)
-        session.add(product2)
-        session.commit()
+        for product in products:
+            session.add(Products(category=product[0], brand=product[1], name=product[2], description=product[3], rate=product[4], price=product[5], image=product[6]))
+            session.commit()
+
 
 def main():
     create_database()
@@ -42,4 +67,3 @@ def search():
         statement = select(Products)
         results = session.exec(statement).all()
         return results
-
